193b5306dfd13e33b5f7aceb6cc972e0b2f12437
Londysiba <londekamhlengi.dlamini19@gmail.com>
2023-12-09 14:30:39 +0200

    updates

--- Javascript/services.js
+++ Javascript/services.js
@@ -3,3 +3,8 @@ function searchLocation() {
     var locationInput = document.getElementById("locationInput").value;
             alert("Searching for event service providers around: " + locationInput);
 }
+
+function toggleSearch() {
+    var searchInput = document.getElementById("searchInput");
+    searchInput.style.display = (searchInput.style.display === 'none' || searchInput.style.display === '') ? 'block' : 'none';
+}
